f747effcaec0f4df178022c8415972b1b9512e17
Ceydaay <cydylmz.99@gmail.com>
2024-08-08 00:21:26 +0300

    Add project files.

---
+++ Constructor/Baby.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConstructor
+{
+    public class BabyClass
+    {
+        public string _name;
+
+        public string _surname;
+
+        public DateTime _birthDate;
+
+
+
+
+
+        public BabyClass()  // Default Constructor without parameters
+        {
+            CryingBaby();
+
+        }
+
+
+        public BabyClass(string name, string surname) // Constructor with two parameters
+
+        {
+            _name = name;
+            _surname = surname;
+
+
+        }
+
+
+        public void CryingBaby()
+
+        {
+            Console.WriteLine("INGAAAAAAAAA");
+
+            _birthDate = DateTime.Now;
+        }
+
+        public void BabyDisplay()
+
+        {
+            Console.WriteLine($" name {_name}, surname {_surname}, birthday {_birthDate}");
+
+           
+        }
+
+
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using PracticeConstructor; 
+{
+
+    BabyClass baby1 = new BabyClass();
+    baby1._name = "ramazan";
+    baby1._surname = "yıldırım";
+
+
+    baby1.BabyDisplay();
+
+
+    BabyClass baby2 = new BabyClass("ramazan", "yıldırım");
+
+
+    baby2.BabyDisplay();
+
+
+
+
+}
